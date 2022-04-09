418983befe0acc4d9efbd8aca8dba049a8a40133
WHJScout <270041283@qq.com>
2022-04-09 19:36:20 +0800

    master

--- print_array.js
+++ print_array.js
@@ -1,4 +1,4 @@
-// Use forEach to console.log contents.
-arr.forEach(function (item2) {
-  console.log(item2);
-});
+// Use a for loop to console.log contents.
+for (var i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
