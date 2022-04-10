a24ce308dbed3604ece1be5b46095832d24f401b
wanghengjing <270041283@qq.com>
2022-04-10 18:57:37 +0800

    delete all files

--- print_array.js
+++
@@ -1,8 +0,0 @@
-// Use a for loop to console.log contents.
-for (var i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
-}
-// Use forEach to console.log contents.
-arr.forEach(function (item3) {
-  console.log(item3);
-});
